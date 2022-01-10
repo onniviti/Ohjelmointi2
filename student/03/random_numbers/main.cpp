1170249ce07a8c0f174082ddf65df35c7960d352
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-10 12:30:15 +0200

    03

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -1,21 +1,41 @@
+
 #include <iostream>
 #include <random>
 #include <string>
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper)
+void produce_random_numbers(unsigned int lower, unsigned int upper, int s)
 {
-    // Implement your function here
+    default_random_engine gen(s);
+    uniform_int_distribution<int> distr(lower, upper);
+
+    bool flag = true;
+    while (flag){
+        cout << "" << endl;
+        std::cout << "Your drawn random number is "
+                  <<distr(gen) << std::endl;
+        string user_input;
+        cout << "Press c to continue or q to quit: ";
+        cin >> user_input;
+        if (user_input == "q")
+            flag = false;
+        else if (user_input == "c")
+            continue;
+
+    }
+
 }
 
 int main()
 {
     unsigned int lower_bound, upper_bound;
+    int s;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
     cin >> upper_bound;
+
 
     if(lower_bound >= upper_bound)
     {
@@ -24,7 +44,10 @@ int main()
         return EXIT_FAILURE;
     }
 
-    produce_random_numbers(lower_bound, upper_bound);
+    cout << "Enter a seed value: ";
+    cin >> s;
+
+    produce_random_numbers(lower_bound, upper_bound,s);
 
     return EXIT_SUCCESS;
 }
