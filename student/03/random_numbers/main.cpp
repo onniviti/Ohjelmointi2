5957e791e874087b5c43796faa5b0ea77e52ac78
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-10 12:33:21 +0200

    random_numbers

--- student/03/random_numbers/main.cpp
+++ student/03/random_numbers/main.cpp
@@ -1,36 +1,41 @@
-
 #include <iostream>
 #include <random>
 #include <string>
 
 using namespace std;
 
-void produce_random_numbers(unsigned int lower, unsigned int upper, int s)
+void produce_random_numbers(unsigned int lower, unsigned int upper, int seed)
 {
-    default_random_engine gen(s);
+
+    default_random_engine gen(seed);
     uniform_int_distribution<int> distr(lower, upper);
 
     bool flag = true;
+
+
     while (flag){
-        cout << "" << endl;
-        std::cout << "Your drawn random number is "
-                  <<distr(gen) << std::endl;
-        string user_input;
-        cout << "Press c to continue or q to quit: ";
-        cin >> user_input;
-        if (user_input == "q")
+        cout << "Your drawn random number is " << distr(gen) << endl;
+        string j;
+        cout << "Press q to quit or any other key to continue: ";
+        cin >> j;
+        cout << endl;
+        if (j == "q"){
             flag = false;
-        else if (user_input == "c")
+
+        }
+        else {
             continue;
+        }
 
     }
 
 }
 
+
+
 int main()
 {
-    unsigned int lower_bound, upper_bound;
-    int s;
+    unsigned int lower_bound, upper_bound, seed;
     cout << "Enter a lower bound: ";
     cin >> lower_bound;
     cout << "Enter an upper bound: ";
@@ -45,9 +50,10 @@ int main()
     }
 
     cout << "Enter a seed value: ";
-    cin >> s;
+    cin >> seed;
+    cout << endl;
 
-    produce_random_numbers(lower_bound, upper_bound,s);
+    produce_random_numbers(lower_bound, upper_bound, seed);
 
     return EXIT_SUCCESS;
 }
