7df062c65cd5848291babcb490e8c3400d1fb095
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-10 12:35:50 +0200

    bank_account

--- student/03/bank_account/account.cpp
+++ student/03/bank_account/account.cpp
@@ -1,13 +1,54 @@
 #include "account.hh"
 #include <iostream>
 
-Account::Account(const std::string& owner, bool has_credit):
+Account::Account(const std::string& owner, bool has_credit):  has_credit_(has_credit), owner_(owner)
 {
     generate_iban();
+
 }
+
+
+
+void Account::set_credit_limit(int amount)
+{
+    if (has_credit_)
+            credit_limit_ = amount;
+        else
+            std::cout << "Cannot set credit limit: the account has no credit card" << std::endl;
+
+}
+
+void Account::save_money(int amount)
+{
+    balance_ += amount;
+}
+
+void Account::take_money(int amount)
+{
+    if (balance_ > amount){
+            balance_ -= amount;
+            std::cout << amount << " euros taken: new balance of " << iban_ << " is "<< balance_
+                 << " euros" << std::endl;
+        }
+        else if (has_credit_){
+            if ((credit_limit_ + balance_) >= amount){
+                balance_ -= amount;
+                std::cout << amount << " euros taken: new balance of " << iban_ << " is "
+                     << balance_ << " euros" << std::endl;}
+            else {
+                std::cout << "Cannot take money: credit limit overflow" << std::endl;
+            }
+
+        }
+        else
+            std::cout << "Cannot take money: balance underflow" << std::endl;
+
+}
+
 
 // Setting initial value for the static attribute running_number_
 int Account::running_number_ = 0;
+
 
 void Account::generate_iban()
 {
@@ -25,4 +66,38 @@ void Account::generate_iban()
 
     iban_ = "FI00 1234 ";
     iban_.append(suffix);
+}
+
+
+
+
+
+void Account::transfer_to(Account &vastaanottaja, int amount)
+{
+    if (balance_ > amount){
+            balance_ -= amount;
+            vastaanottaja.balance_ += amount;
+            std::cout << amount << " euros taken: new balance of " << iban_ << " is "
+                 << balance_ << " euros" << std::endl;
+        }
+        else if (has_credit_){
+            if ((credit_limit_ + balance_) > amount){
+                balance_ -= amount;
+                vastaanottaja.balance_ += amount;
+                std::cout << amount << " euros taken: new balance of " << iban_ << " is "
+                     << balance_ << " euros" << std::endl;}
+            else {
+                std::cout << "Cannot take money: credit limit overflow" << std::endl;
+                std::cout << "Transfer from " << iban_ << " failed" << std::endl;
+        }}
+        else {
+            std::cout << "Cannot take money: balance underflow" << std::endl;
+            std::cout << "Transfer from " << iban_ << " failed" << std::endl;
+        }
+
+}
+
+void Account::print() const
+{
+    std::cout << owner_ << " : " << iban_ << " : " << balance_ << " euros" << std::endl;
 }
--- student/03/bank_account/account.hh
+++ student/03/bank_account/account.hh
@@ -10,6 +10,20 @@ public:
     Account(const std::string& owner, bool has_credit = false);
 
     // More methods
+    void set_credit_limit(int amount);
+
+    void save_money(int amount);
+
+    void take_money(int amount);
+
+    void transfer_to(Account& vastaanottaja, int amount);
+
+    void print() const;
+
+
+
+
+
 
 private:
     // Generates IBAN (based on running_number_ below).
@@ -22,8 +36,16 @@ private:
     // In other words, running_number_ is a class-wide attribute, there is
     // no own copies of it for each object of the class.
     static int running_number_;
+    int credit_limit_ = 0;
+    int balance_ = 0;
+    bool has_credit_ = false;
+    std::string owner_;
+    std::string iban_;
+
 
     // More attributes/methods
 };
+
+
 
 #endif // ACCOUNT_HH
