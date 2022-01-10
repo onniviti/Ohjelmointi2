50c978b284bcfbfe3f195b9224c2756c84084e8c
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-10 12:13:21 +0200

    factors

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <cmath>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Enter a positive number: ";
+    std::string user_input ="";
+
+    std::cout << "Enter a positive number: ";
+    std::cin >> user_input;
+
+    int n = std::stoi(user_input);
+    if (n <= 0){
+        std::cout << "Only positive numbers accepted" << std::endl;
+        return 0;
+
+    }
+
+    int factor_cand = std::max(static_cast<int>(sqrt(n)), 1);
+
+    for (; n % factor_cand != 0; --factor_cand);
+
+
+    int factor1 = factor_cand;
+    int factor2 = n / factor_cand;
+
+
+    std::cout << user_input << " = " << factor1 << " * " <<factor2 << endl;
+
 
     return 0;
 }
