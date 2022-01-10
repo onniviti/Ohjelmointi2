efb22adb2c967ec5c2be687537b1c2975d88d414
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-10 12:09:34 +0200

    number_series_game

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,7 +3,20 @@
 
 int main()
 {
+    int numbers = 0;
+    int i = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> numbers;
 
-    return 0;
-}
+    while ( i < numbers ){
+        i += 1;
+        if ( i % 3 == 0 and i % 7 == 0 ){
+            std::cout << "zip boing" << std::endl; }
+        else if ( i % 3 == 0){
+            std::cout << "zip" << std::endl;}
+        else if ( i % 7 == 0){
+            std::cout << "boing" << std::endl;}
+        else
+            std::cout << i << std::endl;
+        }
+        }
