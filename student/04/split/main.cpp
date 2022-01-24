d2a9aeae7c2419b3083553e46b2655a5c9a730ff
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-01-24 07:44:36 +0200

    split

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -1,10 +1,59 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 
+
+
 // TODO: Implement split function here
 // Do not change main function
+std::vector <std::string> split(std::string merkkijono, char erotinmerkki,
+                                bool totuusarvo = false){
+
+
+    std::string merkit = "";
+    std::vector <std::string> tuote;
+    std::vector <std::string> valmistuote;
+    if (totuusarvo == false){
+        for (char& c :  merkkijono){
+            if (c != erotinmerkki){
+                merkit += c;
+            }
+
+            else {
+                tuote.push_back(merkit);
+                merkit.clear();}
+
+        }
+        tuote.push_back(merkit);
+    return tuote;
+}
+    else {
+        for (char& c :  merkkijono){
+            if (c != erotinmerkki){
+                merkit += c;
+            }
+
+            else {
+                tuote.push_back(merkit);
+                merkit.clear();}
+
+        }
+        tuote.push_back(merkit);
+
+        int maara = tuote.size();
+        for (int i = 0; i < maara; ++i){
+            if (tuote[i] == ""){
+                valmistuote.push_back(tuote[i]);
+                valmistuote.pop_back();
+            }
+            else (valmistuote.push_back(tuote[i]));
+
+        }
+        return valmistuote;
+        }
+    }
 
 
 int main()
