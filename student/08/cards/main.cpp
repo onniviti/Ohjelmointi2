6059eca1694a0f09e46d38f2ff5a316e263862f5
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-03-21 23:29:57 +0200

    cards

--- student/08/cards/main.cpp
+++ student/08/cards/main.cpp
@@ -7,7 +7,7 @@ int main() {
     cout << "constructor" << endl;
     Cards deck;
 
-    int n = 0;
+    auto n = 0;
     cout << "Enter amount of test cards: ";
     cin >> n;
 
@@ -15,9 +15,8 @@ int main() {
     deck.print_from_top_to_bottom(cout);
 
     cout << endl << "add * n" << endl;
-    for(int i = 0; i < n; i++) {
+    for (auto i = 0; i < n; i++)
         deck.add(i);
-    }
 
     cout << endl << "print_from_top_to_bottom" << endl;
     deck.print_from_top_to_bottom(cout);
@@ -35,14 +34,14 @@ int main() {
     cout << endl << "print_from_top_to_bottom" << endl;
     deck.print_from_top_to_bottom(cout);
 
+
     cout << endl << "print_from_bottom_to_top" << endl;
     deck.print_from_bottom_to_top(cout);
 
     cout << endl;
-    int value = 0;
-    while(deck.remove(value)) {
+    auto value = 0;
+    while (deck.remove(value))
         cout << "remove " << value << endl;
-    }
 
     cout << endl << "destructor" << endl;
     return EXIT_SUCCESS;
